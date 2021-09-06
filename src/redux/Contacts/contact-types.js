81099aa560721e73c761401fe81f0f6b1d952c52
Nastya-Lopatina <73726295+Nastya-Lopatina@users.noreply.github.com>
2021-09-07 08:51:50 +0900

    в работе

--- src/redux/Contacts/contact-types.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    ADD_CONTACT: 'ContactForm/addContact',
-    DELETE_CONTACT: 'ContactForm/deleteContact',
-    CHANGE_FILTER: 'ContactForm/changeFilter',
-};
